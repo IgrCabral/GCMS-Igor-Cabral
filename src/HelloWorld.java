8975a84cc23f8cb362fb2d3552bc4c5ce0a5ff59
Igor Cabral <ifcfs@discente.ifpe.edu.br>
2022-08-10 12:34:05 -0300

    passo6 da parte3: modificando diretorio de trabalho

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		System.out.println("uma outra modificação no arquivo!- alterei esta linha de texto");
+		System.out.println("mais uma modificação.");
 		
 		
 
