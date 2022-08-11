31417e4ca0497c7434c63b30e47850874f0263f4
Igor Cabral <ifcfs@discente.ifpe.edu.br>
2022-08-11 14:16:28 -0300

    uma modificação na main que não está na hotfix

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World!!");
 		System.out.println("uma outra modificação no arquivo!- alterei esta linha de texto");
 		System.out.println("mais uma modificação.");
-		
+		System.out.println("uma modificação na main que não está na hotfix.");
 		
 
 	}
