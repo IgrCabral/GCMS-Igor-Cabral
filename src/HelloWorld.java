56802964324f4d2c0791969d5f94853470e0d9d0
Igor Cabral <ifcfs@discente.ifpe.edu.br>
2022-08-11 14:14:07 -0300

    primeiro commit na branch 'hotfix'

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World!!");
 		System.out.println("uma outra modificação no arquivo!- alterei esta linha de texto");
 		System.out.println("mais uma modificação.");
+		System.out.println("realizando um commit na branch 'hotfix'.");
 		
 		
 
